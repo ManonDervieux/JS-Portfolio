f66866e1d9e3a0d08866123ec9fa3cf40d8501d0
ManonDervieux <manon.dervieux1992@gmail.com>
2023-09-22 19:16:01 +0200

    emailjs configuration

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,32 @@ window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
 
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
+        
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID -templateID - #form - publicKey
+    emailjs.sendForm('service_1xm9fai', 'template_swjo69j', '#contact-form', 'YNWtq_vU1OkeqpUxM')
+    .then(() =>{
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //clear input fields
+        contactForm.reset()
+}, () =>{
+    //Show error message
+    contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
