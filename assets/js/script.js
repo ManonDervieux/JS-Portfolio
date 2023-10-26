8e791d90d9ff0a15e93177cb98fdc0c52a16ff64
ManonDervieux <manon.dervieux1992@gmail.com>
2023-10-26 17:43:46 +0200

    changements personnels

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+let data = [
+    {
+    subtitle: 'Site Web',
+    title: 'Portfolio',
+    image:'assets/amg/project-1.jpg',
+    description: 'Projet personnel dont le but est de me présenter. Ce site est compatible Desktop et Smartphone. Une page de contact est inclue.',
+    techno: ['HTML', 'CSS', 'Javascript'],
+    github: 'https://github.com/ManonDervieux/JS-Portfolio',
+    link: '',
+    },
+    {
+    subtitle: 'Site Web',
+    title: 'Curriculum Vitae',
+    image:'assets/amg/project-2.jpg',
+    description: 'Projet personnel afin de coder mon CV avec une interface Desktop et Smartphone. Un export PDF est possible.',
+    techno: ['HTML', 'CSS', 'Javascript'],
+    github: '',
+    link: '',
+    },
+    {
+    subtitle: 'Site Web',
+    title: 'Blog de Lecture',
+    image:'assets/amg/project-1.jpg',
+    description: "Projet en cours de réalisation. Inspiré de ma passion de la lecture, j'ai eu l'envie de créer un blog afin de partager des critiques de mes lectures.",
+    techno: ['HTML', 'CSS', 'Javascript', 'Nodejs', 'MongoDB', 'VueJS'],
+    github: '',
+    link: '',
+    },
+]
+
+console.log(data);
+
+const projectContainer = document.querySelector('.projects-container');
+
+
